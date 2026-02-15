c71e899f97beb78d3f73020b8e24872a4ccd65bb
Kailashsinh <kailashsinhrajput25@gmail.com>
2026-02-15 19:28:19 +0530

    commit

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465, // SSL
-    secure: true,
+    port: 587, // TLS
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
