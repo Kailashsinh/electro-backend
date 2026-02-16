62f9404ac5febc6a7562d022f04a460b024e72fd
Kailashsinh <kailashsinhrajput25@gmail.com>
2026-02-16 10:49:42 +0530

    coommmmit

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,21 +1,19 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: 'smtp-relay.brevo.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    // Force IPv4 to avoid IPv6 connection issues on Render
-    family: 4,
 });
 
 const sendEmail = async (to, subject, html) => {
     try {
         const info = await transporter.sendMail({
-            from: '"ElectroCare" <kailashsinhrajput25@gmail.com>', // sender address
+            from: '"ElectroCare" <a27b05001@smtp-brevo.com>', // Sender address
             to, // list of receivers
             subject, // Subject line
             html, // html body
